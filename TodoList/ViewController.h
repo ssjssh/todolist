e15b6febc14259820773999c70ddcf7da7e4fd1a
shengshijun <ssj970763342@163.com>
2015-03-18 21:18:50 +0800

    todolist 初始提交

---
+++ TodoList/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TodoList
+//
+//  Created by 沈世军 on 15/3/18.
+//  Copyright (c) 2015 沈世军. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by 沈世军 on 15/3/18.
+//  Copyright (c) 2015 沈世军. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
